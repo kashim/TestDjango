184c2924bd463d27d86a629c6a639a2fbe66402a
Kashim <dmetlushko@gmail.com>
2011-07-18 10:25:38 +0300

    add first template

--- src/TestDjango/poll/views.py
+++ src/TestDjango/poll/views.py
@@ -1,11 +1,25 @@
 # Create your views here.
 from django.http import HttpResponse
+#from django.template import Context, loader
+from django.shortcuts import render_to_response
 from models import Poll
 
 def index(request):
     latestPolls = Poll.objects.order_by("-pub_date")[:5]
-    output = '\n'.join( [p.question for p in latestPolls] )
-    return HttpResponse( "My test response \n" + output )
+    return render_to_response( "poll/index.html", { "latest_poll_list" : latestPolls } )
+
+#    ldr = loader.get_template("poll/index.html")
+#    cntx = Context(
+#                   {
+#                    'latest_poll_list' : latestPolls,
+#                   }
+#           )
+#    
+#    return HttpResponse( ldr.render( cntx ) )
+
+#    output = '\n'.join( [p.question for p in latestPolls] )
+#    return HttpResponse( "My test response \n" + output )
+
 
 def detail( request, poll_id ):
     return HttpResponse( "Search for poll id = " + poll_id )
